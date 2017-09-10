cecd2a5281da8355c14ece71b005ca2486bf9892
CDanyluk <chantaldanyluk@yahoo.com>
2017-09-10 13:40:48 -0500

    Revert "Added controller class"
    This reverts commit 310ca113517de1d4ca89c3c44d952fbf501b4cf8.

--- AestheticAPP.java
+++ AestheticAPP.java
@@ -1,33 +1,26 @@
 import javafx.fxml.FXMLLoader;
-
-import java.io.IOException;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.fxml.FXMLLoader;
-import javafx.application.Application;
-import javafx.scene.layout.VBox;
 
 public class AestheticAPP {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		Pane root;
 		try {
-			root = (Pane) FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch (IOException e) {
-			System.out.println("Could not open window!");
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
 
-	public static void main(String[] args) {Launch(args);}
-
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
 
-
+}
--- AestheticController.java
+++
@@ -1,9 +0,0 @@
-import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
-
-public class AestheticController {
-	
-	@FXML
-	private Pane pane;
-	
-}
