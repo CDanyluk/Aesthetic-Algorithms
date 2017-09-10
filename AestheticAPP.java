41acbe21afc261d5f6ee79bf2fb9e5bf85690960
Taylor Baer <baertt@hendrix.edu>
2017-09-10 13:08:02 -0500

    Application launch fix
    Fix errors reported by Chantal

--- AestheticAPP.java
+++ AestheticAPP.java
@@ -1,26 +1,28 @@
+
+import java.awt.ScrollPane;
+
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class AestheticAPP {
-	
+public class AestheticAPP extends Application {
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
-			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+	public void start(Stage primaryStage) throws Exception{
+		FXMLLoader loader = new FXMLLoader();
+		
+		Parent root = FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
+	    Scene scene = new Scene(root);
+
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-}
-
 }
