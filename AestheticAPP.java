76e996e55616a2324255a63d1dca722baa942fbf
CDanyluk <chantaldanyluk@yahoo.com>
2017-09-10 13:40:36 -0500

    Revert "Fuck it"
    This reverts commit 734cdb562f0a1f31e7eea7b7ebde31baeb33cffb.

--- AestheticAPP.java
+++ AestheticAPP.java
@@ -1,8 +1,6 @@
 import javafx.fxml.FXMLLoader;
 
 import java.io.IOException;
-
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
@@ -11,23 +9,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.application.Application;
 import javafx.scene.layout.VBox;
 
-public class AestheticAPP {	
+public class AestheticAPP {
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception{
-		
-		FXMLLoader loader = new FXMLLoader();
-		
-		Parent root = FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
-	    Scene scene = new Scene(root);
-
-
-		primaryStage.setScene(scene);
-		primaryStage.show();
+	public void start(Stage primaryStage) {
+		Pane root;
+		try {
+			root = (Pane) FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			System.out.println("Could not open window!");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
-	public static void main(String[] args) {
-		Launch(args);
-	}
+	public static void main(String[] args) {Launch(args);}
+
 }
+
 
