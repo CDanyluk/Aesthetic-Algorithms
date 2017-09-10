310ca113517de1d4ca89c3c44d952fbf501b4cf8
CDanyluk <chantaldanyluk@yahoo.com>
2017-09-10 13:17:31 -0500

    Added controller class

--- AestheticAPP.java
+++ AestheticAPP.java
@@ -1,26 +1,33 @@
 import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
+import javafx.scene.layout.VBox;
 
 public class AestheticAPP {
 	
 	@Override
 	public void start(Stage primaryStage) {
+		Pane root;
 		try {
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
+			root = (Pane) FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch (Exception e) {
+		} catch (IOException e) {
+			System.out.println("Could not open window!");
 			e.printStackTrace();
 			System.exit(1);
 		}
 	}
 
-	public static void main(String[] args) {
-		launch(args);
-	}
-}
+	public static void main(String[] args) {Launch(args);}
 
 }
+
+
---
+++ AestheticController.java
@@ -0,0 +1,9 @@
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class AestheticController {
+	
+	@FXML
+	private Pane pane;
+	
+}
