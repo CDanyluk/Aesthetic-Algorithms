b302b56737b2cd7679b6ad66a53452edc79c0eb7
Taylor Baer <baertt@hendrix.edu>
2017-09-11 10:19:01 -0500

    Add main

---
+++ src/GUIMain.java
@@ -0,0 +1,27 @@
+
+import java.awt.ScrollPane;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GUIMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+		FXMLLoader loader = new FXMLLoader();
+		
+		Parent root = FXMLLoader.load(getClass().getResource("ViewGUI.fxml"));
+	    Scene scene = new Scene(root);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/MainController.java
@@ -0,0 +1,106 @@
+
+
+import javafx.beans.value.ChangeListener;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class MainController {
+	
+	@FXML
+	RadioButton lSystem;
+	
+	@FXML
+	RadioButton cellularAutomata;
+	
+	@FXML
+	RadioButton cartesianGenetic;
+	
+	final ToggleGroup algorithm = new ToggleGroup();
+	
+	@FXML
+	TextField backgroundColor;
+	
+	@FXML
+	TextField penColor;
+	
+	@FXML
+	TextField iterations;
+	
+	@FXML
+	Button start;
+	
+	@FXML
+	Canvas picture;
+	
+	
+	
+	@FXML 
+	public void initialize() {
+		setButtonGroup();
+		startHandler();
+		
+		
+	}
+	
+	private void startHandler(){
+		start.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+            	checkColor();
+            	draw();
+            	
+            }
+        });
+	}	
+
+	
+	public void setButtonGroup(){
+		lSystem.setToggleGroup(algorithm);
+		cellularAutomata.setToggleGroup(algorithm);
+		cartesianGenetic.setToggleGroup(algorithm);
+		lSystem.setSelected(true);
+	}
+	
+	public void checkColor(){
+		if(backgroundColor.getText().length() != 7 || !backgroundColor.getText().substring(0).equals("#")){
+			try{
+				Integer.parseInt(backgroundColor.getText().substring(1));
+			} catch(NumberFormatException nfe){
+				throwErrorAlert("Please enter a valid background color.");
+			}	
+		}
+		
+		if(penColor.getText().length() != 7 || !penColor.getText().substring(0).equals("#")){
+			try{
+				Integer.parseInt(penColor.getText().substring(1));
+			} catch(NumberFormatException nfe){
+				throwErrorAlert("Please enter a valid pen color.");
+			}	
+		}
+			
+	}
+	
+	private void throwErrorAlert(String msg){
+		Alert alert = new Alert(AlertType.ERROR, msg, ButtonType.OK);
+		alert.showAndWait();
+	}
+	
+	// TODO: Write this method! 
+	// Will draw on the canvas
+	public void draw(){
+		
+	}
+	
+	
+
+
+}
