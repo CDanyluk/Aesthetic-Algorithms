6fe8b38d40f925a26c17de5d66d8d785a67066ec
CDanyluk <chantaldanyluk@yahoo.com>
2017-09-18 10:57:55 -0500

    zdgdz
    zdzdg

--- src/MainController.java
+++ src/MainController.java
@@ -88,6 +88,7 @@ public class MainController {
 		GraphicsContext gc = picture.getGraphicsContext2D();
 		gc.setFill(backcolor.getValue());
 		gc.fillRect(0,0,613,460);
+		//wutwut
 	}
 	
 	public void drawTree(double[] start) {
