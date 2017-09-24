1f5a968fb12365d20994de72269b047aea696df0
CDanyluk <chantaldanyluk@yahoo.com>
2017-09-24 10:36:31 -0500

    Line(double[] first, double[] last)
    Changes made to line class to complete it as storage for two double[] arrays. Functions getFirst and getLast return one of two points and getLength() returns distance between those two points.

--- src/Line.java
+++ src/Line.java
@@ -1,6 +1,30 @@
+import javafx.animation.AnimationTimer;
 
 public class Line {
-	//stores to double[] points
+	//stores two double[] points
 	//fuckyeah abstraction
+	
+	private double[] first;
+	private double[] last;
 
+	
+	public Line(double[] first, double[] last) {
+		this.first = first;
+		this.last = last;
+	}
+	
+	public double[] getFirst() {
+		return first;
+	}
+	
+	public double[] getLast() {
+		return last;
+	}
+	
+	public double getLength() {
+		double dist = Math.sqrt(Math.pow((first[0] - last[0]), 2) + Math.pow((first[1]) - last[1], 2) );
+		return dist;
+	}
+	
+	
 }
