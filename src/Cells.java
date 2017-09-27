3c35844ddc7a9fe2cac21237b28151c10f76989f
CDanyluk <chantaldanyluk@yahoo.com>
2017-09-27 15:53:45 -0500

    Do we really need edge cases?
    Naw!
    Wrote a function to implement John *Coways* game of life. Hasn't been called yet.

--- src/Cells.java
+++ src/Cells.java
@@ -27,6 +27,48 @@ public class Cells {
 		}
 	}
 	
+	public void iterate(int num) {
+		//This will call a function that has rules num times
+		//make num iterations and num different ones
+	}
+	
+	public void conway() {
+		//Create new graph for next iteration
+		//double[][] population = new double[61][46];
+		//This will affect the final chart but should make changes without it
+		for (int x = 0; x < 61; x++) {
+			for (int y = 0; y < 46; y ++) {
+				//Create int neighbors to keep track of x, y's neighbors
+				int neighbors = 0;
+				//Go through all 8 potential neighbors
+				if (graph[x-1][y-1] == 1) {
+					neighbors++;
+				}else if (graph[x][y-1] == 1) {
+					neighbors++;
+				}else if (graph[x+1][y-1] == 1) {
+					neighbors++;
+				}else if (graph[x-1][y] == 1) {
+					neighbors++;
+				}else if (graph[x+1][y] == 1) {
+					neighbors++;
+				}else if (graph[x-1][y+1] == 1) {
+					neighbors++;
+				}else if (graph[x][y+1] == 1) {
+					neighbors++;
+				}else if (graph[x+1][y+1] == 1) {
+					neighbors++;
+				}
+				//if x,y is black and neighbors > 3 turn white
+				if (graph[x][y] == 1 && neighbors > 3) {
+					graph[x][y] = 0;
+				//if x,y is white and neighbors are 4 or 5
+				}else if ((graph[x][y] == 0 && neighbors == 4) || (graph[x][y] == 0 && neighbors == 5)) {
+					graph[x][y] = 1;
+				}
+			}
+		}
+	}
+	
 	public void live(int x, int y) {
 		graph[x][y] = 1;
 	}
