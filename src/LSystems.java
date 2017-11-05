2bcb0acd587e96e15a24deac61f8187795a12f82
Taylor Baer <baertt@hendrix.edu>
2017-11-04 22:11:28 -0500

    Abstraction of L-System

--- src/LSystems.java
+++ src/LSystems.java
@@ -1,12 +1,11 @@
 import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Stack;
 
 public class LSystems {
 	
-	//TODO: implement this in future iterations
-	//private String[] constants;
 	private String axiom; //start
 	private HashMap<String, String> rules;
 	private Stack<Turtle> toDo;
@@ -14,10 +13,12 @@ public class LSystems {
 	private Set<Line> drawing;
 	private double length;
 	private int angle;
+	private boolean randomize;
 	
 	public LSystems(double[] startPoint, 
 			String axiom,
 			HashMap<String, String> rules,
+			boolean randomize,
 			int recursions,
 			double length,
 			int angle){
@@ -27,218 +28,82 @@ public class LSystems {
 		this.drawing = new HashSet<Line>();
 		this.length = length;
 		this.angle = angle;
-		//drawTree("1111[11[1[0]0]1[0]0]11[1[0]0]1[0]0");
-		//drawTree(new Turtle(startPoint[0], startPoint[1], 270), recurseTree(axiom, recursions));
-		drawTriangle(new Turtle(startPoint[0], startPoint[1], 0), recurseTriangle(axiom, recursions));
-		//drawDragon(new Turtle(startPoint[0], startPoint[1], 0), recurseDragon(axiom, recursions));
-		//drawKoch(new Turtle(startPoint[0], startPoint[1], 0), recurseKoch(axiom, recursions));
+		this.randomize = randomize;
+		draw(new Turtle(startPoint[0], startPoint[1], angle), recurse(axiom, recursions, rules), randomize);
+
 	}
 
 
-	public Set<Line> getTree() {
+	public Set<Line> getDrawing() {
 		return drawing;
 	}
 	
-	private String recurseTree(String tree, int recursions){
+	private String recurse(String product, int recursions, HashMap<String, String> rules ){
 		if(recursions == 0){
-			//System.out.println("Final Tree: " + tree);
-			return tree;
+			return product;
 		} else{
-			String treeNew = "";
-			for(int i = 0; i < tree.length(); i++){
-				if(tree.substring(i, i+1).equals("0")){
-					treeNew += rules.get("0");
-				} else if(tree.substring(i, i+1).equals("1")){
-					treeNew += rules.get("1");
+			String productNew = "";
+			for(int i = 0; i < product.length(); i++){
+				String index = product.substring(i, i+1);
+				if(rules.containsKey(index)){
+					productNew += rules.get(index);
 				} else {
-					treeNew += tree.substring(i, i+1);
+					productNew += product.substring(i, i+1);
 				}
 			}
-			return recurseTree(treeNew, recursions - 1);
+			return recurse(productNew, recursions - 1, rules);
 		}
 	}
 	
-	private void drawTree(Turtle first, String tree){
+	private void draw(Turtle first, String toDraw, boolean randomize){
 		toDo.push(first);
-		for(int i = 0; i < tree.length(); i++){
-			if(tree.substring(i, i +1).equals("0")){
-				double[] start = new double[2];
-				start[0] = toDo.peek().x;
-				start[1] = toDo.peek().y;
-				
+		for(int i = 0; i < toDraw.length(); i++){
+			String letter = toDraw.substring(i, i +1);
+			
+			double[] start = new double[2];
+			start[0] = toDo.peek().x;
+			start[1] = toDo.peek().y;
+			
+			if(letter.equals("0")){
 				toDo.peek().goForward(length);
 				double[] end = new double[2];
 				end[0] = toDo.peek().x;
 				end[1] = toDo.peek().y;
 				drawing.add(new Line(start, end));
-			} if(tree.substring(i, i +1).equals("1")){
-				double[] start = new double[2];
-				start[0] = toDo.peek().x;
-				start[1] = toDo.peek().y;
-				
+			} if(letter.equals("1")){
 				toDo.peek().goForward(length);
 				double[] end = new double[2];
 				end[0] = toDo.peek().x;
 				end[1] = toDo.peek().y;
 				drawing.add(new Line(start, end));
-			} if(tree.substring(i, i +1).equals("[")){
-				double[] start = new double[2];
-				start[0] = toDo.peek().x;
-				start[1] = toDo.peek().y;
-				
-				toDo.push(new Turtle(toDo.peek().x, toDo.peek().y, toDo.peek().angle));				
-				toDo.peek().turnLeft(angle);
+			} if(letter.equals("[")){toDo.push(new Turtle(toDo.peek().x, toDo.peek().y, toDo.peek().angle));	
+				if(randomize){
+					int randomNum = ThreadLocalRandom.current().nextInt(5, 100);
+					toDo.peek().turnLeft(randomNum);
+				} else {
+					toDo.peek().turnLeft(angle);
+				}
 				toDo.peek().goForward(length);
 				double[] end = new double[2];
 				end[0] = toDo.peek().x;
 				end[1] = toDo.peek().y;
 				drawing.add(new Line(start, end));
-			} if(tree.substring(i, i +1).equals("]")){
-				double[] start = new double[2];
-				start[0] = toDo.peek().x;
-				start[1] = toDo.peek().y;
-				
+			} if(letter.equals("]")){
 				toDo.pop();
 				toDo.peek().turnLeft(-angle);
 				toDo.peek().goForward(length);
 				double[] end = new double[2];
 				end[0] = toDo.peek().x;
 				end[1] = toDo.peek().y;
-			} 
-		}
-		
-	}
-	
-	private String recurseTriangle(String triangle, int recursions) {
-		if(recursions == 0){
-			//System.out.println("Final product: " + triangle);
-			return triangle;
-		} else{
-			String triangleNew = "";
-			for(int i = 0; i < triangle.length(); i++){
-				if(triangle.substring(i, i+1).equals("A")){
-					//System.out.println("Replaced an F: " + triangle);
-					triangleNew += rules.get("A");
-				} else if(triangle.substring(i, i+1).equals("B")){
-					triangleNew += rules.get("B");
-				} else {
-					triangleNew += triangle.substring(i, i+1);
-				}
-			}
-			return recurseTriangle(triangleNew, recursions - 1);
-		}
-	}
-	
-	private void drawTriangle(Turtle first, String triangle) {
-		toDo.push(first);
-		//Here, F and G both mean "draw forward", + means "turn left by angle", and − means "turn right by angle".
-		for(int i = 0; i < triangle.length(); i++) {
-			if(triangle.substring(i, i+1).equals("A") || triangle.substring(i, i+1).equals("B")) {
-				
-				double[] start = new double[2];
-				start[0] = toDo.peek().x;
-				start[1] = toDo.peek().y;
-				
-				toDo.peek().goForward(length);
-				double[] end = new double[2];
-				end[0] = toDo.peek().x;
-				end[1] = toDo.peek().y;
-				drawing.add(new Line(start, end));
-				//System.out.println(start[0] + ", " + start[1] + " : " + end[0] + ", " + end[1] );
-			} else if(triangle.substring(i, i+1).equals("+")) {
+			} else if(letter.equals("+")) {
 				toDo.peek().turnLeft(angle);
-			} else if(triangle.substring(i, i+1).equals("-")) {
+			} else if(letter.equals("-")) {
 				toDo.peek().turnLeft(-angle);
 			}
 		}
 		
 	}
 	
-	private String recurseDragon(String dragon, int recursions){
-		if(recursions == 0){
-			//System.out.println("Final product: " + dragon);
-			return dragon;
-		} else{
-			String dragonNew = "";
-			for(int i = 0; i < dragon.length(); i++){
-				if(dragon.substring(i, i+1).equals("X")){
-					dragonNew += rules.get("X");
-				} else if(dragon.substring(i, i+1).equals("Y")){
-					dragonNew += rules.get("Y");
-				} else {
-					dragonNew += dragon.substring(i, i+1);
-				}
-			}
-			return recurseDragon(dragonNew, recursions - 1);
-		}
-	}
-	
-	private void drawDragon(Turtle first, String dragon) {
-		toDo.push(first);
-		//Here, F and G both mean "draw forward", + means "turn left by angle", and − means "turn right by angle".
-		for(int i = 0; i < dragon.length(); i++) {
-			if(dragon.substring(i, i+1).equals("F")) {
-				
-				double[] start = new double[2];
-				start[0] = toDo.peek().x;
-				start[1] = toDo.peek().y;
-				
-				toDo.peek().goForward(length);
-				double[] end = new double[2];
-				end[0] = toDo.peek().x;
-				end[1] = toDo.peek().y;
-				drawing.add(new Line(start, end));
-				//System.out.println(start[0] + ", " + start[1] + " : " + end[0] + ", " + end[1] );
-			} else if(dragon.substring(i, i+1).equals("-")) {
-				toDo.peek().turnLeft(angle);
-			} else if(dragon.substring(i, i+1).equals("+")) {
-				toDo.peek().turnLeft(-angle);
-			}
-		}
-		
-	}
-	
-	private String recurseKoch(String koch, int recursions){
-		if(recursions == 0){
-			//System.out.println("Final product: " + koch);
-			return koch;
-		} else{
-			String kochNew = "";
-			for(int i = 0; i < koch.length(); i++){
-				if(koch.substring(i, i+1).equals("F")){
-					kochNew += rules.get("F");
-				} else {
-					kochNew += koch.substring(i, i+1);
-				}
-			}
-			return recurseKoch(kochNew, recursions - 1);
-		}
-	}
-	
-	private void drawKoch(Turtle first, String koch) {
-		toDo.push(first);
-		//Here, F and G both mean "draw forward", + means "turn left by angle", and − means "turn right by angle".
-		for(int i = 0; i < koch.length(); i++) {
-			if(koch.substring(i, i+1).equals("F")) {
-				
-				double[] start = new double[2];
-				start[0] = toDo.peek().x;
-				start[1] = toDo.peek().y;
-				
-				toDo.peek().goForward(length);
-				double[] end = new double[2];
-				end[0] = toDo.peek().x;
-				end[1] = toDo.peek().y;
-				drawing.add(new Line(start, end));
-				//System.out.println(start[0] + ", " + start[1] + " : " + end[0] + ", " + end[1] );
-			} else if(koch.substring(i, i+1).equals("+")) {
-				toDo.peek().turnLeft(angle);
-			} else if(koch.substring(i, i+1).equals("-")) {
-				toDo.peek().turnLeft(-angle);
-			}
-		}
-		
-	}
 	
 	
 }
