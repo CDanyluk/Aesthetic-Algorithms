54a41f5b3b2c490656372408c2ef16cbba17db84
CDanyluk <chantaldanyluk@yahoo.com>
2017-11-12 18:18:03 -0600

    DUmb color changes

--- src/CellPattern.java
+++ src/CellPattern.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import javafx.scene.paint.Color;
 
@@ -21,7 +22,7 @@ public class CellPattern {
 		}
 		this.aliveMap = alive;
 		this.deadMap = dead;
-		randomizeColors(25);
+		rainbow();
 	}
 	
 	//All the getter and setter methods
@@ -42,6 +43,82 @@ public class CellPattern {
 			rainbow.put(i, color);
 		}
 		this.colorMap = rainbow;
+	}
+	
+	public void rainbow () {
+		this.colorMap = makeColorGradient(0.4, 0.4, 0.4, 0, 2, 4);
+	}
+	
+	 public HashMap<Integer, Color> makeColorGradient(double freq1, double freq2, double freq3, int phase1, int phase2, int phase3) {
+		HashMap<Integer, Color> rainbow = new HashMap<Integer, Color>();
+		int center = 128;
+		int width = 127;
+		int len = 50;
+
+		for (int i = 0; i < len; i++) {
+			 double r = Math.sin(freq1*i + phase1) * width + center;
+			 int red = (int)Math.floor(r);
+			 double g = Math.sin(freq2*i + phase2) * width + center;
+			 int green = (int)Math.floor(g);
+			 double b = Math.sin(freq3*i + phase3) * width + center;
+			 int blue = (int)Math.floor(b);
+			 Color temp = Color.rgb(red, green, blue);
+			 rainbow.put(i,  temp);
+		 }
+		return rainbow;
+	 }
+	
+	public void gradient(int max) {
+		
+		Random random = new Random();
+		HashMap<Integer, Color> rainbow = new HashMap<Integer, Color>();
+		Color color = randomColor();
+		rainbow.put(0, Color.AQUAMARINE);
+		//rainbow.put(1, invert(color));
+		for (int i = 1; i < max; i ++) {
+			Color cur = rainbow.get(i-1);
+			int rand = (int)Math.floor(Math.random()*(3-1+1)+1);
+			int updown = 0;
+			if (random.nextBoolean()) {
+			     updown = 1;
+			}
+			else {
+				updown = -1;
+			}
+			int red = (int)cur.getRed() * 255;
+			int green = (int)cur.getGreen() * 255;
+			int blue = (int)cur.getBlue() * 255;
+			//RED = 1
+			if (rand == 1 && red < 250 && red > 5) {
+				int curRed = (int)(cur.getRed() * 255);
+				red = Math.abs(curRed + updown);
+				Color next = Color.rgb(red, green, blue);
+				rainbow.put(i, next);
+			//GREEN = 2
+			}else if (rand == 2 && green < 250 && green > 5) {
+				green = (int)Math.abs(cur.getGreen()  * 255  + updown);
+				Color next = Color.rgb(red, green, blue);
+				rainbow.put(i, next);
+			//BLUE = 3
+			}else if (rand == 3 && blue < 250 && blue > 5){
+				blue = (int)Math.abs(cur.getBlue()  * 255 + updown);
+				Color next = Color.rgb(red, green, blue);
+				rainbow.put(i, next);
+			}else {
+				Color next = randomColor();
+				rainbow.put(i, next);
+			}
+			
+		}
+		this.colorMap = rainbow;
+	}
+	
+	public Color invert(Color c) {
+		int red = (int)c.getRed();
+		int green = (int)c.getGreen();
+		int blue = (int)c.getBlue();
+		Color inverse = Color.rgb(blue, red, green);
+		return inverse;
 	}
 	
 	//Takes a number and randomly changes that many colors in the colorMap
