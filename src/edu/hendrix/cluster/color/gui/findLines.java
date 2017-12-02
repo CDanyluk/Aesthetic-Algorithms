dd42cfe712f8295aedd9c49befcaf6fc77ecd13b
CDanyluk <chantaldanyluk@yahoo.com>
2017-12-02 16:14:52 -0600

    Ints now work
    Because they are doubles now primarily. But it is cutting things in half appropraitely it looks like, getting F/T/T/T for my example image when it should be getting T/T/T/F. The for loops need to be looked at intensely to understand what it is actually doing.

--- src/edu/hendrix/cluster/color/gui/findLines.java
+++ src/edu/hendrix/cluster/color/gui/findLines.java
@@ -46,27 +46,31 @@ public class findLines {
 		int y1 = b.getMinY();
 		int x2 = b.getMaxX();
 		int y2 = b.getMaxY();
-		int H = b.getHeight();
-		int W = b.getWidth();
+		double H = b.getHeight();
+		double W = b.getWidth();
 		int n = 4;
 		Boolean[][] grid = new Boolean[n+1][n+1];
 		int falseNum = 0;
-		for (int j = 0; j <= n; j++) {
-			for (int i = 0; i < n - 1; i++) {
-				int curX = x1+(W*(i/n));
-				int nextX = x1+(W*((i+1)/n));
-				int curY = y1+(H*(j/n));
-				grid[i][j] = isEmpty(curX, curY, nextX, curY, b);
+		double j = 0;
+		//for (int j = 0; j <= n; j++) {
+			for (double i = 0; i < n; i++) {
+				double curX = x1+(W*(i/n));
+				System.out.println("curX : " + curX);
+				double nextX = (x1)+(W*(((i+1)/n)));
+				System.out.println("nextX : " + nextX);
+				double curY = y1+(H*(j/n));
+				double nextY = y1+(H*((j+1)/n));
+				grid[(int)j][(int)i] = isEmpty(curX, nextX, curY, nextY, b);
 			}
-		}
+		//}
 		printGraph(grid);
-		for (int x = 0; x < n; x++) {
+		/*for (int x = 0; x < n; x++) {
 			for (int y = 0; y < n; y++) {
 				if (grid[x][y] == false) {
 					falseNum++;
 				}
 			}
-		}
+		}*/
 		return falseNum >= 6;
 		
 		
@@ -84,22 +88,23 @@ public class findLines {
 	
 	//Instead of checking if every pixel in the box is part of the blob, 
 	//this will only check to see if when cut in the middle both ways it runs across a blob
-	public boolean isEmpty(int x1, int x2, int y1, int y2, Blob b) {
-		int halfX = (x1 + ((x2 - x1)/2));
-		int halfY = (y1 + ((y2 - y1)/2));
+	public boolean isEmpty(double x1, double x2, double y1, double y2, Blob b) {
+		double halfX = (x1 + ((x2 - x1)/2));
+		System.out.println("halfX : " + halfX);
+		double halfY = (y1 + ((y2 - y1)/2));
 		//cut up and down, false if crosses blob
-		for (int i = y1; i <= y2; i ++) {
-			if (blobs.getBlob(halfX, i) != b) {
-				return false;
+		for (double i = y1; i <= y2; i ++) {
+			if (blobs.getBlob((int)halfX, (int)i) == b) {
+				return true;
 			}
 		}
 		//cut left and right and sees if it crosses blob
-		for (int j = x1; j <= x2; j ++) {
-			if (blobs.getBlob(j, halfY) != b) {
-				return false;
+		for (double j = x1; j <= x2; j ++) {
+			if (blobs.getBlob((int)j, (int)halfY) == b) {
+				return true;
 			}
 		}
-		return true;
+		return false;
 	}
 	
 
