eec3f9a609f5f8fa6bc6458fb1884d781702c3ce
CDanyluk <chantaldanyluk@yahoo.com>
2017-12-10 13:08:12 -0600

    FindShapes is gonna kill me
    Its got some enums and a chi-score for each blob according to each shape.
    
    I need to get it to the point where I can test it. I just want to know if it is doing the right things with my weird hashmap inside hashmaps >.<

---
+++ src/edu/hendrix/cluster/color/gui/FindShapes.java
@@ -0,0 +1,130 @@
+package edu.hendrix.cluster.color.gui;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import edu.hendrix.blob.Blob;
+
+public class FindShapes {
+	
+	private WrappedBlobList blobs;
+
+	private HashMap<Blob, HashMap<String, Double>> classBlob;
+	private int imagew;
+	private int imageh;
+	private int n;
+	
+	public FindShapes(int imagew, int imageh, WrappedBlobList b) {
+		this.blobs = b;
+		this.imagew = imagew;
+		this.imageh = imageh;
+		//n is the matrix dimensions : nxn
+		this.n = 6;
+		this.classBlob = new HashMap<Blob, HashMap<String, Double>>();
+		initializeShapes();
+	}
+	
+	public void initializeShapes() {
+		HashMap<String, Double> percentages = new HashMap<String, Double>();
+		for (int i = 0; i < blobs.size(); i++) {
+			Blob b = blobs.get(i);
+			b.setHW();
+			Boolean[][] grid = BlobToMatrix(b);
+			//Compare whether the blob is a line and store the value
+			double line = gridComparison(getLine(), grid);
+			percentages.put("line", line);
+			//compare whether the blob is a square and store the value
+			double square = gridComparison(getSquare(), grid);
+			percentages.put("square", square);
+			//compare whether the blob is a triangle and store the value
+			
+			//compare whether the blob is a circle and store the value
+			
+			//Check to see if it is a line without matrix
+			if (straightCheck(b)) {
+				//this means it is absolutely a line, or 1/1
+				//override previous value
+				percentages.put("line", 1.0);
+			}
+			classBlob.put(b, percentages);
+		}
+	}
+	
+	public double gridComparison(Boolean[][] expected, Boolean[][] given) {
+		//initialize the number of squares different
+		int dif = 0;
+		//Go through and find the number of squares different
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (expected[i][j] != given[i][j]) {
+					dif++;
+				}
+			}
+		}
+		//What is the chi-square statistic?
+		double chi = 1/(dif)+1;
+		return chi;
+		
+	}
+	
+	public boolean straightCheck(Blob b) {
+		return (b.getWidth()*1.0/imagew < 0.05) || (b.getHeight()*1.0/imageh < 0.05);
+	}
+	
+	//Takes a blob and return a 6x6 matrix version of it
+	public Boolean[][] BlobToMatrix(Blob b) {
+		int x1 = b.getMinX();
+		int y1 = b.getMinY();
+		int x2 = b.getMaxX();
+		int y2 = b.getMaxY();
+		double H = b.getHeight();
+		double W = b.getWidth();
+		Boolean[][] grid = new Boolean[n+1][n+1];
+		int falseNum = 0;
+		int trueNum = 0;
+		for (double i = 0; i < n; i++) {
+			for (double j = 0; j < n; j++) {
+				double curX = x1+(W*(i/n));
+				double nextX = (x1)+(W*(((i+1)/n)));
+				double curY = y1+(H*(j/n));
+				double nextY = y1+(H*((j+1)/n));
+				grid[(int)j][(int)i] = isEmpty(curX, nextX, curY, nextY, b);
+			}
+		}
+		return grid;
+		
+	}
+	
+	public void printGraph(Boolean[][] graph, int num) {
+		for (int x = 0; x < num; x++) {
+			for (int y = 0; y < num; y ++) {
+			
+				System.out.print(graph[x][y]+ " ");
+			}
+			System.out.print("\n");
+		}
+		System.out.println("---------------------");
+	}
+	
+	//Instead of checking if every pixel in the box is part of the blob, 
+	//this will only check to see if when cut in the middle both ways it runs across a blob
+	public boolean isEmpty(double x1, double x2, double y1, double y2, Blob b) {
+		double halfX = (x1 + ((x2 - x1)/2));
+		double halfY = (y1 + ((y2 - y1)/2));
+		//cut up and down, false if crosses blob
+		for (double i = y1; i <= y2; i ++) {
+			if (blobs.getBlob((int)halfX, (int)i) == b) {
+				return true;
+			}
+		}
+		//cut left and right and sees if it crosses blob
+		for (double j = x1; j <= x2; j ++) {
+			if (blobs.getBlob((int)j, (int)halfY) == b) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
---
+++ src/edu/hendrix/cluster/color/gui/Shapes.java
@@ -0,0 +1,95 @@
+package edu.hendrix.cluster.color.gui;
+
+public enum Shapes {
+	
+	LINE ("line"){
+		public Boolean[][] getGrid() { return getLine(); }
+		
+	},
+	SQUARE("square"){
+		public Boolean[][] getGrid() { return getSquare(); }
+		
+	},
+	TRIANGLE("triangle"){
+		public Boolean[][] getGrid() { return getTriangle(); }
+		
+	},CIRClE("circle"){
+		public Boolean[][] getGrid() { return getCircle(); }
+		
+	};
+	
+	public abstract Boolean[][] getGrid();
+	
+	private final String name;
+	
+	Shapes(String name) {
+		this.name = name;
+	}
+	
+	String getName() {
+		return name;
+	}
+
+	//grid for line
+	public Boolean[][] getLine() {
+		int n = 6;
+		Boolean[][] grid = new Boolean[n+1][n+1];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				grid[j][i] = false;
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			grid[i][i] = true;
+		}
+		return grid;
+	}
+	
+	//Grid for square
+	public Boolean[][] getSquare() {
+		int n = 6;
+		Boolean[][] grid = new Boolean[n+1][n+1];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				grid[j][i] = true;
+			}
+		}
+		return grid;
+	}
+	
+	//Grid for triangle
+	public Boolean[][] getTriangle() {
+		int n = 6;
+		Boolean[][] grid = new Boolean[n+1][n+1];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				grid[j][i] = true;
+			}
+		}
+		grid[0][0] = false; grid[1][0] = false; grid[4][0] = false; grid[5][0] = false;
+		grid[0][1] = false; grid[1][1] = false; grid[4][1] = false; grid[5][1] = false;
+		grid[0][2] = false; grid[1][2] = false; grid[4][2] = false; grid[5][2] = false;
+		grid[0][3] = false; grid[5][3] = false;
+		grid[0][4] = false; grid[5][4] = false;
+		
+		return grid;
+		
+	}
+	
+	//Grid for circle 
+	public Boolean[][] getCircle() {
+		int n = 6;
+		Boolean[][] grid = new Boolean[n+1][n+1];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				grid[j][i] = true;
+			}
+		}
+		grid[0][0] = false; grid[0][5] = false;
+		grid[5][0] = false; grid[5][5] = false;
+		
+		return grid;
+		
+	}
+	
+}
